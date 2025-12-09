0b13b56827f950646e3199b2b7011d57e910c327
Spar-b <spartanfastfun@gmail.com>
2025-12-09 09:14:38 +0200

    Added migation, fixed bugs

---
+++ Migrations/20251209071305_AddProductEmbedding.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Pgvector;
+
+#nullable disable
+
+namespace BoardGamesStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductEmbedding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Vector>(
+                name: "embedding",
+                table: "products",
+                type: "vector(384)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "embedding",
+                table: "products");
+        }
+    }
+}
